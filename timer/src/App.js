2d2d12cc92a4e9fc9262546d644b1415e9f55231
FioraS <60643571+FioraS@users.noreply.github.com>
2020-02-05 17:47:56 +0100

    Update App.js

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
-import Map from "./components/map/Map";
+import Timer from './components/timer/Timer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-	  <Map center={ { lat: 45.1885, lng: 5.7245} } />
-    </div>
-  );
-}
+  
+  function App() {
+    return (
+     <div className="App">
+      
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Timer/>
+          
+          <a className="App-link">
+            Learn React
+          </a>
+        </header>
+     
+      </div>
+    );
+  }
+
 
 export default App;
