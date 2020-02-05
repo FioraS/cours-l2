52286b02c4488432fa97c252e5d51d1ec52729d0
FioraS <60643571+FioraS@users.noreply.github.com>
2020-02-05 17:46:45 +0100

    Create user.js

---
+++ layout/src/user.js
@@ -0,0 +1,15 @@
+const posts =
+[{
+    "name" : "Yacine",
+    "age" : "19",
+    "genre" : "big noob"
+},
+
+{
+    "name" : "fiora",
+    "age" : "19",
+    "genre" : "PGM"
+}
+]
+
+export default posts
