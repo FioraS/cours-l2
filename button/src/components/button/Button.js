e407e1e8323eba2aafeee7cd7608911a1aa07c81
FioraS <60643571+FioraS@users.noreply.github.com>
2020-02-05 17:44:06 +0100

    Update Button.js

--- button/src/components/button/Button.js
+++ button/src/components/button/Button.js
@@ -1,30 +1,35 @@
 import React from "react";
+/*import logo from './logo.svg';*/
+import ReactDOM from 'react-dom';
 import './Button.css';
 
 class Button extends React.Component {
 
   constructor(props) {
     super(props);
-    // ...
+     this.state={isToggle: false};
+    this.handleClick = this.handleClick.bind(this);
+   
   }
 
   handleClick() {
-    // ...
+     this.setState((oldState) => ({
+      isToggle: ! oldState.isToggle
+    }));
   }
 
   render() {
-    return (
-      <button
-         data-testid="button"
-         { /*
-            A remplir
-         */ }
-      >
-      {this.props.children ? this.props.children : "Add text!"}
-      </button>);
-  }
-
+  return (
+    <div className="App">
+	  <button
+	  onClick={this.handleClick}
+	  className= {this.state.isToggle ? 'toggled' : 'untoggled' }>
+	  {this.state.isToggle ? 'toggled' : 'untoggled'}
+	  </button>
+    </div>
+  );
 }
 
+}
 
 export default Button;
