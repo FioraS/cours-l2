da628dab4210bc1ee32b47f64ab48b083dfc0574
FioraS <60643571+FioraS@users.noreply.github.com>
2020-02-05 17:48:25 +0100

    Update Timer.js

--- timer/src/components/timer/Timer.js
+++ timer/src/components/timer/Timer.js
@@ -4,21 +4,25 @@ class Timer extends React.Component {
 
   constructor(props) {
     super(props);
-    // ...
+    this.state={seconds:0};
   }
 
   componentDidMount() {
-    // ...
+    this.interval = 
+    setInterval(() => this.setState(state => ({
+      seconds: state.seconds + 1
+    })), 
+    1000);
   }
 
   componentWillUnmount() {
-    // ...
+    clearInterval(this.interval);
   }
   
   render () {
     return (
       <div data-testid="timer">
-        {this.state.counter}
+        {this.state.seconds}
       </div>
     );
   }
