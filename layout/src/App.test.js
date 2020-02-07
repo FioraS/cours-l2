e1ed2787b4fd1785cd0de711550c7d9b958eb310
FioraS <60643571+FioraS@users.noreply.github.com>
2020-02-07 13:51:32 +0100

    Update App.test.js

--- layout/src/App.test.js
+++ layout/src/App.test.js
@@ -7,3 +7,23 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+beforeEach( () => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach( () => {
+  unmountComponentAtNode(container);
+  container.remove();
+});
+
+it('renders without crashing', () => {
+  act( () => {
+    render(<BtnInfo></BtnInfo>, container);
+  });
+  expect((document.querySelector("[data-testId='btnInfo']")).textContent).toMatch("Plus d'information")
+
+
+});
